02c8557437547c766e07161f04c4e47c842a2f0b
KauaBordin <kauabordin2005@gmail.com>
2024-10-30 21:34:24 -0300

    ...

--- Lista5/Ex.1.c
+++ Lista5/Ex.1.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 int main()
 {
-    int A[10];
-    srand(time(0));
-    printf("A) ");
-    for(int i = 0; i < 10; i++)
-    {
-        A[i] = (rand() % 81) + 10;
-        printf("%d ", A[i]);
-    }
+
+
+
+
+
+    return 0;
 }
